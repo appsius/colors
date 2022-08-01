66054bac509a15b76111ac5d2bc5d66e232c54b0
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-01 18:57:34 +0300

    created react app & added palette

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar goes here */}
+        <div className="Palette-colors">{/* a bunch of boxes */}</div>
+        {/* footer ll be here */}
+        <h1>Palette</h1>
+      </div>
+    );
+  }
+}
