4895c3701c6292015e59d9853d3039e73ea755ab
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-14 07:22:21 +0300

    Add styles on mini palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 export default function PaletteList(props) {
   const { palettes } = props;
   return (
     <div>
+      <MiniPalette />
+      <h1>React Colors</h1>
       {palettes.map((palette) => (
         <p>
           <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
