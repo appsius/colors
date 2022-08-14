409a9978940ae41f34a443908045b081b19de851
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-14 11:06:56 +0300

    Add single color box

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ export default function App() {
     <Routes>
       <Route exact path='/' element={<PaletteList palettes={seedColors} />} />
       <Route exact path='/palette/:paletteId' element={<Palette />} />
+      <Route
+        exact
+        path='/palette/:paletteId/:colorId'
+        element={<h1>Single Color Palette</h1>}
+      />
     </Routes>
   );
 }
