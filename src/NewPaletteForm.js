f0ad8a898fae82ce84a547d720e4bb65434484b7
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-18 15:53:36 +0300

    Add color picker and buttons

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,10 @@ import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { ChromePicker } from 'react-color';
+import { Button } from '@mui/material';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
@@ -105,6 +107,22 @@ export default function NewPaletteForm() {
           </IconButton>
         </DrawerHeader>
         <Divider />
+        <Typography variant='h4'>Design Your Palette</Typography>
+        <div>
+          <Button variant='contained' color='error'>
+            Clear Palette
+          </Button>
+          <Button variant='contained' color='primary'>
+            Random Color
+          </Button>
+        </div>
+        <ChromePicker
+          color={'purple'}
+          onChangeComplete={(newColor) => console.log(newColor)}
+        />
+        <Button variant='contained' color='primary'>
+          Add Color
+        </Button>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
