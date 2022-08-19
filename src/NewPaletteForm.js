b8991486a2206c26adce7d4735a23377fdc80e4a
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-19 19:18:55 +0300

    Start to style palette nav

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
@@ -33,23 +32,6 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
     }),
   })
 );
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -128,9 +110,7 @@ export default function NewPaletteForm({ palettes, savePalette, classes }) {
     <Box sx={{ display: 'flex' }}>
       <PaletteFormNav
         open={open}
-        classes={classes}
         palettes={palettes}
-        AppBar={AppBar}
         handleSubmit={handleSubmit}
         handleDrawerOpen={handleDrawerOpen}
       />
--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,18 +1,47 @@
 import React, { useState } from 'react';
+import MuiAppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { styled } from '@mui/material/styles';
 import { Button } from '@mui/material';
+import { withStyles } from '@mui/styles';
 import { TextValidator, ValidatorForm } from 'react-material-ui-form-validator';
 import { Link } from 'react-router-dom';
 
+const drawerWidth = 400;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  height: '64px',
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const styles = {
+  Root: {},
+  navBtns: {},
+};
+
 function PaletteFormNav({
-  classes,
   open,
   palettes,
-  AppBar,
+  classes,
   handleSubmit,
   handleDrawerOpen,
 }) {
@@ -33,7 +62,7 @@ function PaletteFormNav({
   };
 
   return (
-    <div>
+    <div className={classes.Root}>
       <CssBaseline />
       <AppBar position='fixed' open={open} color='default'>
         <Toolbar>
@@ -49,6 +78,8 @@ function PaletteFormNav({
           <Typography variant='h6' noWrap component='div'>
             Persistent drawer
           </Typography>
+        </Toolbar>
+        <div className={classes.navBtns}>
           <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
             <TextValidator
               label='Palette Name'
@@ -64,16 +95,16 @@ function PaletteFormNav({
             <Button variant='contained' color='primary' type='submit'>
               Save Palette
             </Button>
-            <Link to='/'>
-              <Button variant='contained' color='error'>
-                Go Back
-              </Button>
-            </Link>
           </ValidatorForm>
-        </Toolbar>
+          <Link to='/'>
+            <Button variant='contained' color='error'>
+              Go Back
+            </Button>
+          </Link>
+        </div>
       </AppBar>
     </div>
   );
 }
 
-export default PaletteFormNav;
+export default withStyles(styles, { theme: true })(PaletteFormNav);
