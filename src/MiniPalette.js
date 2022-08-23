996b189387c4e55d46fca54318ec3ac7e7c76a43
appsius <ilyas.ustunkaya234434@gmail.com>
2022-08-23 14:08:17 +0300

    Add delete icon to mini palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@mui/styles';
 import { styles } from './styles/MiniPaletteStyles';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function MiniPalette(props) {
   const { classes, paletteName, emoji, colors, handleClick } = props;
@@ -14,6 +15,14 @@ function MiniPalette(props) {
 
   return (
     <div className={classes.root} onClick={handleClick}>
+      <div>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          style={{
+            transition: 'all .3s ease-in-out',
+          }}
+        />
+      </div>
       <div className={classes.colors}>{miniBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -7,7 +7,12 @@ const styles = {
     padding: '0.5rem',
     position: 'relative',
     overflow: 'hidden',
-    '&:hover': { cursor: 'pointer' },
+    cursor: 'pointer',
+    '&:hover': {
+      '& $deleteIcon': {
+        opacity: 1,
+      },
+    },
   },
   colors: {
     backgroundColor: '#dae1e4',
@@ -37,6 +42,18 @@ const styles = {
     margin: '0 auto',
     position: 'relative',
     marginBottom: '-4.5px',
+  },
+  deleteIcon: {
+    color: 'white',
+    backgroundColor: '#eb3d30',
+    width: '20px',
+    height: '20px',
+    position: 'absolute',
+    right: '0px',
+    top: '0px',
+    padding: '10px',
+    zIndex: 100000,
+    opacity: 0,
   },
 };
 
